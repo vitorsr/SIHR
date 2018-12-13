cc520d3eb0d48263dcc817996848b73132c1b758
Vítor Ramos <vitorsr@ufrn.edu.br>
2018-12-13 16:21:48 -0300

    Same for this one

--- qx/qx.m
+++ qx/qx.m
@@ -1,9 +1,15 @@
 classdef qx
     properties (Constant)
         THR    = 0.03
-        SIGMAS =  3.0
-        SIGMAR =  0.1
+        SIGMAS = 3.0
+        SIGMAR = 0.1
         SZ = 2*ceil(2*qx.SIGMAS)+1
+    end
+    methods(Static)
+        function [src,dst] = main(fname)
+            src = im2double(imread(fname));
+            [src,dst] = qxHighlightRemoval(src);
+        end
     end
 end
 
