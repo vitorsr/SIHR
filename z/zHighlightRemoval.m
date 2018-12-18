8bb51b0cc69a412f7252980bba34984eb68dc11d
Vítor Ramos <vitorsr@ufrn.edu.br>
2018-12-18 09:04:38 -0300

    Fixed #1, renamed some files, added new test image and rewrote some scripts

--- z/zHighlightRemoval.m
+++ z/zHighlightRemoval.m
@@ -1,6 +1,7 @@
 function [img,sfi,diff] = zHighlightRemoval(fname,sz)
-img.rgb = double(imresize(imread(fname),sz,'lanczos3'));
-img.rgb = padarray(img.rgb,[1 1],'symmetric','post');
+img.rgb = double(imresize(imread(fname),sz,'Method','bilinear'));
+img.rgb(end+1,1:end,:) = img.rgb(end,1:end,:); % pad post
+img.rgb(1:end,end+1,:) = img.rgb(1:end,end,:);
 img.i   = zeros([size(img.rgb,1) size(img.rgb,2)],'uint8');
 [sfi,diff] = zRemoveHighlights(img);
 img = img.rgb(1:end-1,1:end-1,:);
