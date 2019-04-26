c21d1f1ba806bcaec99b7c250afc1f45e3612aa9
Vítor Ramos <vitorsr@ufrn.edu.br>
2019-04-26 12:16:46 -0300

    Small edits. Removed unused file

--- Tan2005/zHighlightRemoval.m
+++ Tan2005/zHighlightRemoval.m
@@ -1,5 +1,5 @@
-function [img,sfi,diff] = zHighlightRemoval(fname,sz)
-img.rgb = double(imresize(imread(fname),sz,'Method','bilinear'));
+function [img,sfi,diff] = zHighlightRemoval(fname)
+img.rgb = double(imread(fname));
 img.rgb(end+1,1:end,:) = img.rgb(end,1:end,:); % pad post
 img.rgb(1:end,end+1,:) = img.rgb(1:end,end,:);
 img.i   = zeros([size(img.rgb,1) size(img.rgb,2)],'uint8');
