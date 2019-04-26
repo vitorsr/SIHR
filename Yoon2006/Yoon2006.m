9255daeadf7bbdf66511895696e5eaacc04655b7
Vítor Ramos <vitorsr@ufrn.edu.br>
2019-04-26 13:56:15 -0300

    Fixes #3: Yoon, Akashi and Yamamoto dependencies; replaced some lines in Yamamoto to implement changes in journal paper
    TODO: search area and binarization (redo Section 3.1, Part 4)

--- Yoon2006/Yoon2006.m
+++ Yoon2006/Yoon2006.m
@@ -5,7 +5,13 @@ I = im2double(imread('synth.ppm'));
 Isf = I - min(I,[],3);
 % figure(2), imshow(Isf)
 %% Get its chroma
-c = getChroma(Isf);
+den = repmat(sum(Isf,3),[1 1 3]);
+zero = den==0;
+c = zeros(size(Isf));
+c(~zero) = Isf(~zero)./den(~zero);
+clear den zero
+%%
+% c = getChroma(Isf);
 cr = c(:,:,1);
 cg = c(:,:,2);
 %% Dimensions
@@ -19,8 +25,8 @@ skip = false([nRow*nCol 1]);
 %% Chroma threshold values (color discontinuity)
 thR = 0.05; thG = 0.05;
 %% Iterates until only diffuse pixels are left
-count = uint8(0);
-iter = uint8(0);
+count = uint16(0);
+iter = uint16(0);
 while true
     for x1 = 1:nRow*nCol-1
         x2 = x1 + 1;
@@ -53,7 +59,7 @@ while true
             count = count + 1;
         end
     end
-    if count == 0 || iter == 255
+    if count == 0 || iter == 1000
         break
     end
     count = 0;
@@ -62,11 +68,10 @@ end
 %% Display diffuse image
 Idiff = reshape(I_col,[nRow nCol nCh]);
 figure(3), imshow(Idiff)
-%% Helper function
-function cI = getChroma(I)
-sI = repmat(sum(I,3),[1 1 3]);
-val = sI<1e-3;
-cI = zeros(size(I));
-cI(~val) = I(~val)./sI(~val);
-end
 
+% function cI = getChroma(I)
+% sI = repmat(sum(I,3),[1 1 3]);
+% zeroI = sI==0;
+% cI = zeros(size(I));
+% cI(~zeroI) = I(~zeroI)./sI(~zeroI);
+% end
---
+++ utils/Saturate.m
@@ -0,0 +1,3 @@
+function Y = Saturate(X)
+Y = min(1,max(0,X));
+end
